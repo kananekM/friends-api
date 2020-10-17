fb201b34d52e0f12ac4cba2f058fd06304277fb1
Maymeowz <70834963+manymoji@users.noreply.github.com>
2020-10-17 15:23:21 +0700

    Add many

--- app.js
+++ app.js
@@ -7,5 +7,7 @@ app.get('/', (req, res) => {
 app.get('/miwtoo', (req, res) => {
 	res.send('Sawaddee, I am miwtoo. Not pokemon!');
 });
-
+app.get('/many', (req, res) => {
+	res.send('Howdy! I am Many!!!!!');
+});
 module.exports = app;
